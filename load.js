c3f8f6abbd7ddf11443d1674e343ad64f0e2500d
PineappleRind <pineapplerind.info@gmail.com>
2021-05-25 17:42:23 -0400

    Color updates + add new memes

--- load.js
+++ load.js
@@ -1,10 +1,12 @@
 var bod = document.querySelector("main");
-let things = ["Zhou", "Scuzi", "Grahy", "Heather's Kitchen Party", "Be One with the Pie Crust","Bad dad ad that make me sad, i am crab with a pad", "Fat Christina", "Maryolin!", "Ma name's BRITNEY", "Fabian", "At the business centre, Mike Hepple", "Marcello & Vivian Murphy", "Maurice", "John & Gladys", "Deep Dave", "Grapey", "C-H-E-A-P-O-U-L-E-A-R-N-I cheapoulearni", "Hoooooie", "Canala! Canala!", "TITONGO GET NO PAY", "Chocolo-fudge peanuto-caramel", "Andrew Chester", "Region of Prescott", "Rambleberry", "Aureara Borealis", "Guy Sax"];
+let things = ["BOYBOY","Furball","Meanig","Zhou", "Scuzi", "Grahy", "Heather's Kitchen Party", "Be One with the Pie Crust","Bad dad ad that make me sad, i am crab with a pad", "Fat Christina", "Maryolin!", "Ma name's BRITNEY", "Fabian", "At the business centre, Mike Hepple", "Marcello & Vivian Murphy", "Maurice", "John & Gladys", "Deep Dave", "Grapey", "C-H-E-A-P-O-U-L-E-A-R-N-I cheapoulearni", "Hoooooie", "Canala! Canala!", "TITONGO GET NO PAY", "Chocolo-fudge peanuto-caramel", "Andrew Chester", "Region of Prescott", "Rambleberry", "Aureara Borealis", "Guy Sax"];
 function $(e) {
     return document.getElementById(e)
 }
+let hue = 0;
 function clr() {
-    return `hsl(${Math.random() * 365},70%,50%)`
+    hue = hue + 10
+    return `hsl(${hue},70%,50%)`
 }
 function mergeSort(e) {
     let t = e.length / 2;
