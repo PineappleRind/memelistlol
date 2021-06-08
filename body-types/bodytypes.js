3eb49b032172004485e2c6a69e58961b27faa6eb
PineappleRind <pineapplerind.info@gmail.com>
2021-06-08 15:26:35 -0400

    if this doesn't work *sob*

--- body-types/bodytypes.js
+++ body-types/bodytypes.js
@@ -8,18 +8,21 @@ for (let i = 0; i < $('td').length; i++) {
   console.log(i)
 }
 
+  let modalWrap = $('.modal-wrap')[0]
+  let modal = $('.modal')[0]
+    modalWrap.onclick = closeModal
+a
 function openModal(e,t){
   let html = `
   <h1>Body Type: ${e}</h1>
   <p>${t}</p>
   `
-  let modalWrap = $('.modal-wrap')[0]
-  let modal = $('.modal')[0]
+
   modal.innerHTML = html
   modalWrap.classList.add('open')
   modal.classList.add('open')
   
-  modalWrap.onclick = closeModal
+
 }
 
 function closeModal(){ let modalWrap = $('.modal-wrap')[0];
