2d8a5e8dde8156d6017fa2c49ef7eccf4978f5d9
PineappleRind <pineapplerind.info@gmail.com>
2021-06-08 14:01:05 -0400

    Create bodytypes.js

---
+++ body-types/bodytypes.js
@@ -0,0 +1,20 @@
+function $(id){return document.querySelectorAll(id)}
+
+for (let i = 0; i < $('td').length; i++) {
+  $('td')[i].onclick = () => {
+    let desc = $('td').getAttribute('description')
+    openModal($('td').innerHTML,desc)
+  }
+}
+
+function openModal(e,t){
+  let html = `
+  <h1>Body Type: ${e}</h1>
+  <p>${t}</p>
+  `
+  let modalWrap = $('.modal-wrap')[0]
+  let modal = $('.modal')[0]
+  modal.innerHTML = html
+  modalWrap.classList.add('open')
+  modal.classList.add('open')
+}
